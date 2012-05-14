9baaeb2de5b6d70648c8fe24bc7e57975d018d35
Adam Albrecht & Jesse Trimble <pair+adam+jesse@factorylabs.com>
2012-05-14 19:17:35 +0000

    Add a proper spec helper for using Capybara+Rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,14 @@ ENV["RAILS_ENV"] = "test"
 
 $:.unshift File.dirname(__FILE__)
 require "rails_app/config/environment"
+require 'rspec/rails'
+require 'rspec/autorun'
+
+require "capybara/rspec"
+require "capybara/rails"
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+end
