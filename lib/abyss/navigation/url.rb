f8f1fcf0d0173ec67d6d877171c1f1d773a23844
Jesse Trimble <jlowelltrim@gmail.com>
2012-05-23 14:39:42 -0600

    Documentation

--- lib/abyss/navigation/url.rb
+++ lib/abyss/navigation/url.rb
@@ -20,6 +20,11 @@ module Abyss
 
       def href
         begin
+          # The link's name should be the method name
+          # for a rails route helper. We're including the route
+          # helpers in an initializer, so this will only fail if
+          # the route is undefined.
+          #
           send(name)
         rescue NoMethodError => e
           raise ::Abyss::Errors::InvalidUrlHelperMethod.new(name)
